6077aa53929c3bdf8bd16546a442f0a442434477
Ross <ross@snapbill.com>
2012-07-12 10:22:40 +0200

    Changed path in script to ~/dotfiles

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/.dotfiles                    # dotfiles directory
+dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim_runtime xmonad xsession"    # list of files/folders to symlink in homedir
 
