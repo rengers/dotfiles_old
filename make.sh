243dbb61e488f5535f09870f372b303cc63fe429
Ross <rossi.eng@gmail.com>
2013-02-19 00:24:06 +0200

    Added zshrc and ssh-agent scripts

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_logout profile vimrc vim xmonad xsession"    # list of files/folders to symlink in homedir
+files="zshrc zshrc_ssh_agent bashrc bash_logout profile vimrc vim xmonad xsession"    # list of files/folders to symlink in homedir
 
 ##########
 
